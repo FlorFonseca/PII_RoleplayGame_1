955b4a3d899f8027fc777f6c893f519c165c34cd
Martina Laventure <martina.laventure@correo.ucu.edu.uy>
2023-09-21 16:47:19 -0300

    correccion

--- src/Library/Item/MagicHat.cs
+++ src/Library/Item/MagicHat.cs
@@ -31,7 +31,7 @@ namespace RPG
             this.HealValue=healvalue;
         }
 
-        public int getHealvalue()
+        public int getHealValue()
         {
             return HealValue;
         }
