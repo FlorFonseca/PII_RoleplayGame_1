faadc67b970eac515790a3dd9664b320bac16ed8
Martina Laventure <martina.laventure@correo.ucu.edu.uy>
2023-09-30 21:23:46 -0300

    Agrego un enemigo

---
+++ src/Library/Character/Sullivan.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace RPG
+{
+    public class Sullivan : BadGuys
+    {
+        public int victoryPoint=30;
+        public int Strength = 3;
+        public int Intelligence = 2;
+        private int HealthPoint;
+        public override string Name { get; set; }
+
+        public override int GetIntelligence()
+        {
+            return Intelligence;
+        }
+
+        public override int GetStrength()
+        {
+            return Strength;
+        }
+
+        public Sullivan(string name, int healthPoint)
+        {
+            this.Name=name;
+            this.HealthPoint=healthPoint;
+        }
+        
+        public override void SetHealthPoint(int healthPointValue)
+        {
+            this.HealthPoint = healthPointValue;
+        }
+        public override int GetHealthPoint()
+        {
+            return this.HealthPoint;
+        }
+
+        public override int GetVictoryPoints()
+        {
+            return this.victoryPoint;
+        }
+    }
+}
